3abb78fe85b43f89e42a104a6ba76dddb0d6f1b8
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-04-29 12:17:26 +0200

    first commit

---
+++ src/test/java/com/ksidelta/pg/PgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksidelta.pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

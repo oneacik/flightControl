c679e6ba462f642db29ef8de4a376733cbd5734e
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-04-29 20:00:17 +0200

    Stub: FlightAggregate

--- src/main/java/com/ksidelta/pg/model/flight/FlightAggregate.java
+++ src/main/java/com/ksidelta/pg/model/flight/FlightAggregate.java
@@ -7,5 +7,42 @@ import com.ksidelta.pg.model.flight.prices.FlightPrices;
 public class FlightAggregate {
     FlightId flightId;
     FlightPrices flightPrices;
-    FlightItinerary flightItenary;
+    FlightItinerary flightItinerary;
+
+    private FlightAggregate() {
+    }
+
+    public FlightId getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(FlightId flightId) {
+        this.flightId = flightId;
+    }
+
+    public FlightPrices getFlightPrices() {
+        return flightPrices;
+    }
+
+    public void setFlightPrices(FlightPrices flightPrices) {
+        this.flightPrices = flightPrices;
+    }
+
+    public FlightItinerary getFlightItinerary() {
+        return flightItinerary;
+    }
+
+    public void setFlightItinerary(FlightItinerary flightItinerary) {
+        this.flightItinerary = flightItinerary;
+    }
+
+    public static FlightAggregate createFlightAggregate(FlightId flightId, FlightItinerary flightItinerary, FlightPrices flightPrices) {
+        final var flightAggregate = new FlightAggregate();
+
+        flightAggregate.setFlightId(flightId);
+        flightAggregate.setFlightItinerary(flightItinerary);
+        flightAggregate.setFlightPrices(flightPrices);
+
+        return flightAggregate;
+    }
 }
